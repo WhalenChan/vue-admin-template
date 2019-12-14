44561f3daf54cddc8fa7da0d4339b19601464b8d
whalenchan <whalenchan330@gmail.com>
2019-12-14 14:22:06 +0800

    test commit

--- src/main.js
+++ src/main.js
@@ -15,6 +15,8 @@ import router from './router'
 import '@/icons' // icon
 import '@/permission' // permission control
 
+// '@/' 见jsconfig.js =>"@/*": ["src/*"]
+
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
@@ -39,5 +41,6 @@ new Vue({
   el: '#app',
   router,
   store,
+  // 视图渲染
   render: h => h(App)
 })
