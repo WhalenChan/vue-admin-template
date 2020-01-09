8f71a30c7d743400576182c539c558cf455e581a
whalenchan <whalenchan330@gmail.com>
2020-01-09 14:35:56 +0800

    增加/src/layout/components/Sidebar/Item.vue注释

--- src/layout/components/Sidebar/Item.vue
+++ src/layout/components/Sidebar/Item.vue
@@ -1,3 +1,6 @@
+<!-- 注意这个.vue文件没有template模板，但是有render函数 -->
+<!-- 模板 -> 进行编译 -> 生成ast树 -> 数据绑定 -> 成render函数 -> 成虚拟dom -> 真实dom -->
+<!-- 如果直接使用render函数，就省略了模板的编译过程，vue运行的更快 -->
 <script>
 export default {
   name: 'MenuItem',
@@ -12,6 +15,8 @@ export default {
       default: ''
     }
   },
+  // 实际上这个函数替代了模板的作用
+  // 写模板可读性好 但是需要编译 性能略差 不写模板可读性较差 但是不需要编译 性能好
   render(h, context) {
     const { icon, title } = context.props
     const vnodes = []
